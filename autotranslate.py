b779d79f379d27a2bf0ec8311efad29e2a7b53b8
FaceDeer <derksenmobile@gmail.com>
2020-11-01 22:48:52 -0700

    better variable names

--- autotranslate.py
+++ autotranslate.py
@@ -8,7 +8,7 @@ import os, re, shutil
 
 pattern_tr_filename = re.compile(r'\.tr$')
 pattern_tr_id = re.compile(r'\.([^.]*)\.tr$')
-pattern_line_to_translate = re.compile(r'^([^#].*[^@])=$')
+pattern_line_to_translate = re.compile(r'^([^#].*[^@])=$') #finds lines that don't have a translation
 
 translator = Translator()
 
@@ -24,37 +24,37 @@ def translate(tr_filename):
         print("language ID " + lang_id + " is not supported by Google Translate's API")
         return
 
-    lines = [] # this list of strings will ultimately be sent to Google for translation
+    lines_to_translate = [] # this list of strings will ultimately be sent to Google for translation
     with open(tr_filename, "r") as tr_file_handle:
         for line in tr_file_handle:
             # Look for lines that end in "=", ie, that don't have a valid translation added to them
-            line_to_translate = pattern_line_to_translate.search(line)
-            if line_to_translate:
+            line_lacking_translation = pattern_line_to_translate.search(line)
+            if line_lacking_translation:
                 #break the line up at @n markers, this is not ideal for Google
                 #as it may remove some context but it's necessary to allow the
                 #@n markers to be preserved in the output later
-                lines = lines + line_to_translate.group(1).split("@n")
+                lines_to_translate = lines_to_translate + line_lacking_translation.group(1).split("@n")
 
         # Remove duplicates, and the empty string (a common artefact of splitting)
-        line_set = set(lines)
+        line_set = set(lines_to_translate)
         line_set.discard("")
-        lines = list(line_set)
+        lines_to_translate = list(line_set)
 
         # Only do more work if there are lines in need of translation
-        if lines:
+        if lines_to_translate:
             print("Calling Google API for " + lang_id)
-            output = translator.translate(lines, src="en", dest=lang_id)
+            output = translator.translate(lines_to_translate, src="en", dest=lang_id)
 
             #convert the output translations into a dictionary for easy substitution later
-            out_dictionary = dict()
+            translation_dictionary = dict()
             for out_line in output:
-                out_dictionary[out_line.origin] = out_line.text
+                translation_dictionary[out_line.origin] = out_line.text
 
             tr_file_handle.seek(0)
             with open(tr_filename + ".temp", "w", encoding="utf-8") as tr_file_new:
                 for line in tr_file_handle:
-                    line_to_translate = pattern_line_to_translate.search(line)
-                    if line_to_translate:
+                    line_lacking_translation = pattern_line_to_translate.search(line)
+                    if line_lacking_translation:
                         tr_file_new.write("#WARNING: AUTOTRANSLATED BY GOOGLE TRANSLATE\n")
 
                         line = line.rstrip('\n') #remove trailing newline so we can add the translated string to the same line
@@ -64,15 +64,15 @@ def translate(tr_filename):
                         #After splitting the line up on @n again, as was done before, we should have
                         #line segments that match the strings that were sent to Google.
                         for line_piece in line_split:
-                            if line_piece in out_dictionary:
-                                tr_file_new.write(out_dictionary[line_piece])
+                            if line_piece in translation_dictionary:
+                                tr_file_new.write(translation_dictionary[line_piece])
                             else:
                                 #This is valid, the various @n delimiters end up here
                                 tr_file_new.write(line_piece)
                         tr_file_new.write("\n")
                     else:
                         tr_file_new.write(line)
-            shutil.move(tr_filename + ".temp", tr_filename)
+            shutil.move(tr_filename + ".temp", tr_filename) # Overwrite the original file with the new one
 
 
 #If there are already .tr files in /locale, returns a list of their names
